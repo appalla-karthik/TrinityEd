0bb44e586f132e1cd4e9557dcd3bc3c32a6e21fa
Chavda Rajveersinh <rajveer.studenttdb@gmail.com>
2025-09-13 15:48:51 +0530

    first commit

---
+++ TrinityEd_project/TrinityEd_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TrinityedAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'TrinityEd_app'
---
+++ TrinityEd_project/TrinityEd_app/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+form
+
+urlpatterns = [
+     path('', views.home, name='home')
+]
